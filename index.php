7f69f9df1d25dd5e16d1f6d7eb585e4ac5c9e9d3
SredesIntegradas <alansoosa@hotmail.com>
2024-07-17 14:07:38 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+
+    <title>ARI</title>
+    <link rel="stylesheet" href="CSS/estilo.css"/>
+    <style>
+       </style>
+       <meta name="keywords"content="RECECOP, INICIO, PROCESO, OBJETIVOS">
+       <meta name="description"content="Proyecto integrador infrestructura de red 2">
+       <meta name="Author" content="Zahira,Mayra, Alejandro, Alan, Javier">
+       <meta http-equiv="refresh" content="35">
+       <meta charset="UTF-8">
+       <meta name="viewport" content="width=device-width, initial-scale=1.0">  
+       
+</head>
+<body>
+    <header>
+        <h1>ASOCIACIÓN DE REDES INTEGRADAS</h1>
+    </header>
+    <div class="container">
+        <section class="intro">
+            <h2>RECECOP</h2>
+            <p>Conectando el futuro con innovación, tecnología y rehabilitación.</p>
+        </section>
+        <section class="cards">
+            <div class="card">
+                <img src="IMAGENES/723b0c26e651df4342c5f60afc3f1935.gif" alt="mano IA">
+                <div class="card-content">
+                    <h3>INICIO</h3>
+                    <div align="justify"><p><p class="comic"><i>La tecnología innova a un ritmo acelerado, cada año esta se renueva y actualiza a fin de ser más útil y crear una vida más cómoda para el usuario final.</div></i></p>
+                </div>
+            </div>
+            <div class="card">
+                <img src="IMAGENES/8b8259c5dd868e8874d1c12a430d91d2.gif" alt="Computadora">
+                <div class="card-content">
+                    <h3>Acerca de nosotros</h3>
+                    <div align="justify"><p><p class="comic"><i>-Nuestra empresa fue creada con la finalidad de generar ideas y soluciones efectivas para la rehabilitación de centros de cómputo; todo esto con ayuda de la Universidad Tecnológica de Tecámac ya que por medio del proyecto integrador del tercer cuatrimestre se llevó a cabo la
+                         primera propuesta de solución para la Escuela Preparatoria Oficial Núm. 36.</div></i></p>
+                         <a href='./PAGES/objetivos.php'>Nuestros Objetivos</a>
+                </div>
+            </div>
+            <div class="card">
+                <img src="IMAGENES/7e97c2a5c22dcee50af840b1fca2277e.gif" alt="Computadora">
+                <div class="card-content">
+                    <h3>PROCESO</h3>
+                    <div align="justify"><p><p class="comic"><i>-Visitar el centro de cómputo a rehabilitar.</div></i></p>
+                    <div align="justify"><p><p class="comic"><i>-Realizar un inventario de los equipos de cómputo.</div></i></p>
+                    <div align="justify"><p><p class="comic"><i>-Planificación y diseño.</div></i></p>
+                    <div align="justify"><p><p class="comic"><i>-Adquisición de materiales.</div></i></p>
+                    <div align="justify"><p><p class="comic"><i>-Implementación e instalación de software.</div></i></p>
+                    <a href='./PAGES/metas.php'>Conocenos más</a>
+                </div>
+            </div>
+        </section>
+    </div>
+    <nav>
+
+    </nav>
+    <footer>
+        <p>&copy; 2024 ASOCIACIÓN DE REDES INTEGRADAS. Todos los derechos reservados.       
+            <div align="center"><p><p class="comic"><i>Redes sociales</div></i></p>
+        <a href="https://www.facebook.com/rececop?mibextid=ZbWKwL"target="_blank">Facebook </a></ul></div>
+
+        <a href="https://www.instagram.com/ari_rececop?igsh=MXB0bnFsM2FuMTA5dA=="target="_blank">Instagram </a></ul></div>
+       
+        <a href="./PAGES/encuesta.php"target="_blank">Encuesta de satisfaccion</a></ul>
+    
+        <a href='./PAGES/cita.php'>Generar cita</a></div>
+    </footer>
+</body>
+</html>
